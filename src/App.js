17e5b0f4cb30a5857580aa115bd3bdc35959a95e
codelaghien <codelaghien.github.io>
2021-03-11 07:51:22 -0800

    Router

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+	return (
+		<Router>
+			<div>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+					<li>
+						<Link to='/contact'>Contact</Link>
+					</li>
+				</ul>
+
+				<hr />
+				<Switch>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+					<Route path='/home'>
+						<Home />
+					</Route>
+					<Route path='/about'>
+						<About />
+					</Route>
+					<Route path='/contact'>
+						<Contact />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Contact extends React.Component {
+	// constructor(props) {
+	// 	super(props);
+	// }
+
+	render() {
+		return (
+			<div>
+				<h2>Contact</h2>
+			</div>
+		);
+	}
+}
+
+export default Contact;
